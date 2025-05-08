9be0209fb26a5cf35d13d87ab956947a8ded1f68
muynuddinr <muynuddinr@gmail.com>
2025-05-08 12:08:51 +0530

    docker error

--- next.config.js
+++ next.config.js
@@ -17,12 +17,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
